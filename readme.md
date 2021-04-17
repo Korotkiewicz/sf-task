7413e63ef717a94b8b02b350b1cd5f678005382a
Bolebor <korotm@gmail.com>
2021-04-17 17:12:54 +0200

    Add req: func test done

--- readme.md
+++ readme.md
@@ -3,12 +3,17 @@
 ## Task content
 
 Look into [Task file](task.md)
-## Start project from scratch
+## Start project
+
+1. From scratch:
 
     docker-compose build
     docker-compose up -d
     docker-compose exec php composer install
 
+2. Next time:
+
+    docker-compose up -d
 ## Execute recruitement task 
 
 Go into browser and open url: http://localhost:8080/
@@ -17,6 +22,16 @@ There are two links which work:
 - http://localhost:8080/health 
 - http://localhost:8080/file/{filename.json|txt}
 
-## Execute tests (not done yet)
+## Execute tests
+
+If you want execute all tests:
 
     docker-compose exec php php bin/phpunit
+
+If you want execute only functional tests:
+
+    docker-compose exec php php bin/phpunit tests/Controller
+
+If you want execute only unit tests:
+
+    docker-compose exec php php bin/phpunit tests/Service
--- tests/Controller/IndexControllerTest.php
+++ tests/Controller/IndexControllerTest.php
@@ -3,37 +3,106 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class IndexControllerTest extends WebTestCase
 {
-    public function testHealthGetMethod()
+    const ACTION_HEALTH = 'health';
+
+    public function testHomePageRedirect()
     {
         $client = static::createClient();
 
-        $client->request('GET', '/health');
+        $client->request('GET', '/');
+        $baseUrl = $client->getRequest()->getUri();
 
-        $this->assertEquals(204, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isRedirect(), 'Home page should redirect');
+
+        $this->assertResponseRedirects($baseUrl . self::ACTION_HEALTH, 302, 'Home page should redirect to health action');
     }
 
-    public function testHealthNotGetMethod()
+    public function testUnknownPage()
     {
         $client = static::createClient();
 
-        $client->request('POST', '/health');
+        $client->request('GET', '/unknown');
 
-        $this->assertEquals(405, $client->getResponse()->getStatusCode());
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
 
-        $client->request('PUT', '/health');
+    public function testHealthAllowedMethod()
+    {
+        $client = static::createClient();
 
-        $this->assertEquals(405, $client->getResponse()->getStatusCode());
+        $client->request('GET', self::ACTION_HEALTH);
 
-        $client->request('PATCH', '/health');
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $client->getResponse()->getStatusCode());
+    }
 
-        $this->assertEquals(405, $client->getResponse()->getStatusCode());
+    public function testHealthNotAllowedMethod()
+    {
+        $client = static::createClient();
+        $methods = ['POST', 'PUT', 'PATCH', 'DELETE'];
 
-        $client->request('DELETE', '/health');
+        foreach ($methods as $method) {
+            $client->request($method, self::ACTION_HEALTH);
 
-        $this->assertEquals(405, $client->getResponse()->getStatusCode());
+            $this->assertEquals(Response::HTTP_METHOD_NOT_ALLOWED, $client->getResponse()->getStatusCode());
+        }
+    }
 
+    public function testGetFileWithWrongParams()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/file');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+
+        $client->request('GET', '/file/test');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+
+        $client->request('GET', '/file/test.test');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+
+
+    public function testGetFileWithCorrectParams()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/file/test.txt');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testGetFileReturnCorrectFile()
+    {
+        $client = static::createClient();
+
+        $now = new \DateTime;
+        $fileName = 'test_' . $now->getTimestamp() . '.txt';
+        $client->request('GET', '/file/' . $fileName);
+
+        $binaryData = $client->getInternalResponse()->getContent();
+
+        $this->assertNotEmpty($binaryData);
+
+        $client->request('GET', '/file/' . $fileName);
+
+        $secondBinaryData = $client->getInternalResponse()->getContent();
+
+        $this->assertSame($binaryData, $secondBinaryData, 'Two request for same file should return same content.');
+
+        $jsonDecodedData = json_decode($secondBinaryData, true);
+
+        $this->assertNotNull($jsonDecodedData, 'Content is not proper json');
+
+
+        $expectedData = ['file_name' => $fileName, 'datetime' => $now->format('Y-m-d H:i:s')];
+
+        $this->assertEqualsCanonicalizing($expectedData, $jsonDecodedData, 'Content is not correct.');
     }
 }
